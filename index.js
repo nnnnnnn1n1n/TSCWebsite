cde276ba38948fd5145610918352f018d3108a63
nnnnnnn1n1n <natjiraanankul@gmail.com>
2022-05-20 12:27:24 +0700

    3 files modified

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h2 class="text-center">จำหน่ายอุปกรณ์ก่อสร้างที่หลากหลาย</h2>`;
+
+const yearDiv = document.getElementById('year');
+const d = new Date();
+let year = d.getFullYear();
+yearDiv.innerHTML = year;
